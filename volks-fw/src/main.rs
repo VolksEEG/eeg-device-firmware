c3fca109f6bd1e78c3c3b8d5db9ba35c40d76be0
Graham Long <38405059+longevity-software@users.noreply.github.com>
2021-11-30 17:14:06 +0000

    USB Serial is working now in the idle task.

--- volks-fw/src/main.rs
+++ volks-fw/src/main.rs
@@ -12,10 +12,18 @@ mod app {
     use {crate::consts::HEAP_SIZE, cortex_m_rt};
     use {
         nrf52840_hal as hal,
-        pc_communications_hal::pc_coms_hal::CommunicationsInterface as pc,
+        pc_communications_hal::pc_coms_hal::{CommunicationsInterface},
         rtt_target::{rprintln, rtt_init_print},
         systick_monotonic::*,
+        hal::{
+            usbd::{Usbd, UsbPeripheral}, 
+            clocks::{Clocks, ExternalOscillator, Internal, LfOscStopped}
+        },
+        usb_device::{bus::UsbBusAllocator, device::{UsbDeviceBuilder, UsbVidPid}},
+        usbd_serial::{SerialPort, DefaultBufferStore, USB_CLASS_CDC},
     };
+
+    type UsbDevice<'a> = usb_device::device::UsbDevice<'static, Usbd<UsbPeripheral<'a>>>;
 
     #[monotonic(binds = SysTick, default = true)]
     type MyMono = Systick<100>; // 100 Hz / 10 ms granularity
@@ -25,59 +33,104 @@ mod app {
 
     #[local]
     struct Local {
-        pc_interface: pc,
+        usb_dev: UsbDevice<'static>,
+        serial_port: SerialPort<'static, Usbd<UsbPeripheral<'static>>, DefaultBufferStore, DefaultBufferStore>,
+        pc_interface: CommunicationsInterface,
     }
 
     #[init]
     fn init(cx: init::Context) -> (Shared, Local, init::Monotonics) {
         rtt_init_print!();
         rprintln!("[Init]");
-
+        
+        // Initialise the allocator if heap is enabled.
+        #[cfg(feature = "heap")] {
+            rprintln!("Initialising heap allocation!");
+            unsafe {
+                base::ALLOCATOR.init(cortex_m_rt::heap_start() as usize, HEAP_SIZE);
+            }
+        }
         let systick = cx.core.SYST;
         let mono = Systick::new(systick, 64_000_000);
 
-        pc_coms_task::spawn_after(100.millis()).unwrap();
+        let pc_interface = CommunicationsInterface::new();
+        
+        static mut CLOCKS: Option<Clocks<ExternalOscillator, Internal, LfOscStopped>> = None;
+        static mut USB_BUS: Option<UsbBusAllocator<Usbd<UsbPeripheral<'static>>>> = None;
+        
+        let clocks = Clocks::new(cx.device.CLOCK).enable_ext_hfosc();
+        let usbd = cx.device.USBD;
 
-        let pc_interface = pc::new();
-
-        (
-            Shared {},
-            Local {
-                pc_interface,
-            },
-            init::Monotonics(mono),
-        )
+        unsafe {
+            CLOCKS = Some(clocks); 
+            let clocks = CLOCKS.as_ref().unwrap();
+            USB_BUS = Some(Usbd::new(UsbPeripheral::new(usbd, &clocks)));
+            let usb_bus = USB_BUS.as_ref().unwrap();
+            let mut serial_port = SerialPort::new(&usb_bus);
+            let mut usb_dev = UsbDeviceBuilder::new(usb_bus, UsbVidPid(0x16c0, 0x27dd))
+                .manufacturer("Volks EEG")
+                .product("Serial port")
+                .serial_number("TEST")
+                .device_class(USB_CLASS_CDC)
+                .max_packet_size_0(64) // (makes control transfers 8x faster)
+                .build();         
+ 
+            (
+                Shared {},
+                Local {
+                    usb_dev,
+                    serial_port,
+                    pc_interface,
+                },
+                init::Monotonics(mono),
+            )   
+        }
     }
 
-    #[idle]
-    fn idle(_: idle::Context) -> ! {
+    #[idle(local = [pc_interface, usb_dev, serial_port])]
+    fn idle(mut _ctx: idle::Context) -> ! {
+
+        let usb_d = &mut _ctx.local.usb_dev;
+        let srl_prt = &mut _ctx.local.serial_port;
+        let pc = _ctx.local.pc_interface;
+        
         loop {
-            cortex_m::asm::wfi();
-        }
-    }
+            
+            let data = pc.get_data();
 
-    #[task(local = [pc_interface])]
-    fn pc_coms_task(mut _cx: pc_coms_task::Context) {
-
-        let tx = false; //_cx.local.pc_interface.transmit_data();
-        let rx = _cx.local.pc_interface.get_data();
-
-        if rx.0 {
-            // data received
-            // do something with it here 
-
-        }
-
-        match tx || rx.0 {
-            true => { 
-                // data has been received or transmitted to spawn 
-                // again incase there is more to receive or transmit
-                pc_coms_task::spawn().unwrap(); 
+            if data.0 {
+                // there is data to send, so send it
+                srl_prt.write(&[data.1, 1]);
             }
-            false => { 
-                // nothing transmitted or received, so wait a bit
-                pc_coms_task::spawn_after(100.millis()).unwrap(); 
+            
+            if usb_d.poll(&mut [*srl_prt]) {
+                let mut buf = [0u8; 64];
+
+                match srl_prt.read(&mut buf) {
+                    Ok(count) if count > 0 => {
+                        // Echo back in upper case
+                        for c in buf[0..count].iter() {
+
+                            match *c as char {
+                                'f' => {
+                                    pc.transmit_data('I' as u8);
+                                    pc.transmit_data('t' as u8);
+                                    pc.transmit_data(' ' as u8);
+                                    pc.transmit_data('w' as u8);
+                                    pc.transmit_data('o' as u8);
+                                    pc.transmit_data('r' as u8);
+                                    pc.transmit_data('k' as u8);
+                                    pc.transmit_data('s' as u8);
+                                }
+                                _ => {}
+                            }
+                        }
+                    }
+                    _ => {}
+                }     
             }
-        };
+
+            //cortex_m::asm::wfi();
+        }
     }
 }
