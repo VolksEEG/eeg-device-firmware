343f8109f5a2fd4045f31c45fbbc892e780b653e
Graham Long <38405059+longevity-software@users.noreply.github.com>
2021-12-04 21:33:53 +0000

    Removed reliance on dynamic memory allocation and thus the nightly release.

--- pc-communications-hal/src/pc_coms_hal.rs
+++ pc-communications-hal/src/pc_coms_hal.rs
@@ -1,27 +1,27 @@
 //! Abstraction layer around the PC Communications interface
 
 use nrf52840_hal::usbd::{UsbPeripheral, Usbd};
-use ringbuf::{RingBuffer, Producer, Consumer};
 use usb_device::device::UsbDevice;
 use usbd_serial::{DefaultBufferStore, SerialPort};
 
-/// Result of the background task function 
+/// Result of the background task function
 /// this may not be necessary
-pub enum BackgroundTaskResult{
+pub enum BackgroundTaskResult {
     Complete,
     MoreToDo,
 }
 
 /// Structure returned from the get_available_data function
 pub struct RxStruct {
-    pub BytesAvailable: u8,
-    pub DataArray: [u8; 64]
+    pub bytes_available: u8,
+    pub data_array: [u8; 64],
 }
 
 /// Definition of the hardware interface to be used by this module
 pub struct HardwareInterface {
     pub usb_dev: UsbDevice<'static, Usbd<UsbPeripheral<'static>>>,
-    pub serial_port: SerialPort<'static, Usbd<UsbPeripheral<'static>>, DefaultBufferStore, DefaultBufferStore>,    
+    pub serial_port:
+        SerialPort<'static, Usbd<UsbPeripheral<'static>>, DefaultBufferStore, DefaultBufferStore>,
 }
 
 /// PcInterfaceTrait which defines the interface to the modlue.
@@ -38,118 +38,221 @@ pub struct PcInterface {
 
     // private
     hw_interface: HardwareInterface,
-    tx_producer: Producer<u8>,
-    tx_consumer: Consumer<u8>,
-    
-    rx_producer: Producer<u8>,
-    rx_consumer: Consumer<u8>,
+    tx_buffer: [u8; 128],
+    tx_ip: u8,
+    tx_op: u8,
+    tx_count: u8,
+    rx_buffer: [u8; 128],
+    rx_ip: u8,
+    rx_op: u8,
+    rx_count: u8,
+}
+
+/// Private implementations for PcInterface
+impl PcInterface {
+    fn add_byte_to_rx_buffer(&mut self, data: u8) -> bool {
+        if self.rx_count >= 128 {
+            return false;
+        }
+
+        // add the byte to the buffer
+        self.rx_buffer[self.rx_ip as usize] = data;
+
+        // increment and wrap the index
+        self.rx_ip = if self.rx_ip + 1 == 128 {
+            0
+        } else {
+            self.rx_ip + 1
+        };
+
+        // increment the count
+        self.rx_count += 1;
+
+        return true;
+    }
+
+    fn get_byte_from_rx_buffer(&mut self) -> (bool, u8) {
+        if 0 == self.rx_count {
+            return (false, 0);
+        }
+
+        // get the next byte in the buffer
+        let result = self.rx_buffer[self.rx_op as usize];
+
+        // increment and wrap the index
+        self.rx_op = if self.rx_op + 1 == 128 {
+            0
+        } else {
+            self.rx_op + 1
+        };
+
+        // decrement the count
+        self.rx_count -= 1;
+
+        // return the data
+        return (true, result);
+    }
+
+    fn add_byte_to_tx_buffer(&mut self, data: u8) -> bool {
+        if self.tx_count >= 128 {
+            return false;
+        }
+
+        // add the byte to the buffer
+        self.tx_buffer[self.tx_ip as usize] = data;
+
+        // increment and wrap the index
+        self.tx_ip = if self.tx_ip + 1 == 128 {
+            0
+        } else {
+            self.tx_ip + 1
+        };
+
+        // increment the count
+        self.tx_count += 1;
+
+        return true;
+    }
+
+    fn get_byte_from_tx_buffer(&mut self) -> (bool, u8) {
+        if 0 == self.tx_count {
+            return (false, 0);
+        }
+
+        // get the next byte in the buffer
+        let result = self.tx_buffer[self.tx_op as usize];
+
+        // increment and wrap the index
+        self.tx_op = if self.tx_op + 1 == 128 {
+            0
+        } else {
+            self.tx_op + 1
+        };
+
+        // decrement the count
+        self.tx_count -= 1;
+
+        // return the data
+        return (true, result);
+    }
 }
 
 /// Implementing the PcInterfaceTrait for the PcInterface structure
 impl PcInterfaceTrait for PcInterface {
-
     /// new:  creates a new insctance of PcInterface using the passed Hardware Interface
     fn new(hw_interface: HardwareInterface) -> PcInterface {
-        
-        // set up the transmit buffers
-        let tx_buffer = RingBuffer::<u8>::new(128);
-        let (tx_producer, tx_consumer) = tx_buffer.split();
-
-        // and the receive buffers
-        let rx_buffer = RingBuffer::<u8>::new(128);
-        let (rx_producer, rx_consumer) = rx_buffer.split();
-
         // create the struct to return.
         PcInterface {
             hw_interface,
-            tx_producer,
-            tx_consumer,
-            rx_producer,
-            rx_consumer,
+            tx_buffer: [0u8; 128],
+            tx_ip: 0,
+            tx_op: 0,
+            tx_count: 0,
+            rx_buffer: [0u8; 128],
+            rx_ip: 0,
+            rx_op: 0,
+            rx_count: 0,
         }
     }
 
-    /// perform_backgroud_tasks: 
-    /// Performs background tasks for the USB interface. 
+    /// perform_backgroud_tasks:
+    /// Performs background tasks for the USB interface.
     fn perform_backgroud_tasks(&mut self) -> BackgroundTaskResult {
+        // temporary rx buffer.
+        let mut buf = [0u8; 64];
+        let mut count = 0;
 
-        // local references to the hardware interface.
-        let usb_d = &mut self.hw_interface.usb_dev;
-        let srl_prt = &mut self.hw_interface.serial_port;
+        let tx_data = self.get_byte_from_tx_buffer();
 
-        // Poll the USB device for any received data 
-        let rx = if usb_d.poll(&mut [srl_prt]) {
-            let mut buf = [0u8; 64];
+        let tx: bool;
+        let rx: bool;
 
-            // read the data
-            match srl_prt.read(&mut buf) {
-                Ok(count) if count > 0 => {
-                    // Add all bytes to the rx_producer
-                    for elem in buf[0..count].iter() {
-                        self.rx_producer.push(*elem);
+        // scoping brackets to split the mutable self instances required for
+        // the usb_d and srl_prt variables from the instances required by
+        // GetByteFromTxBuffer and AddByteToRxBuffer.
+        {
+            // local references to the hardware interface.
+            let usb_d = &mut self.hw_interface.usb_dev;
+            let srl_prt = &mut self.hw_interface.serial_port;
+
+            // Poll the USB device for any received data
+            rx = if usb_d.poll(&mut [srl_prt]) {
+                // read the data
+                match srl_prt.read(&mut buf) {
+                    Ok(cnt) if cnt > 0 => {
+                        count = cnt;
+                        true
                     }
+                    _ => false,
                 }
-                _ => {}
-            }
-            // data has been recieved so set rx to true
-            true
-        } else {
-            // no data recieved so set rx to false
-            false
-        };
+            } else {
+                // no data recieved so set rx to false
+                false
+            };
 
-        let tx = if self.tx_consumer.is_empty() {
-            // no data to transmit so set tx to false
-            false
-        } else {
+            tx = if tx_data.0 {
+                // write the data to the serial port
+                srl_prt.write(&[tx_data.1, 1]);
 
-            // get the next byte to send
-            let value = self.tx_consumer.pop();
+                // data has been sent so set tx to true
+                true
+            } else {
+                // no data to transmit so set tx to false
+                false
+            };
+        }
 
-            // write it to the serial port
-            if value.is_some() {
-                srl_prt.write(&[value.unwrap(), 1]);
-            }
+        // copy all the received bytes into the rx buffer.
+        for elem in buf[0..count].iter() {
+            self.add_byte_to_rx_buffer(*elem);
+        }
 
-            // data has been sent so set tx to true
-            true
-        };
-
-        // if data has been received or transmitted then there may be more data to recieve 
+        // if data has been received or transmitted then there may be more data to recieve
         // or transmit, so return that there is more to do, otherwise say we are complete.
-        return if tx || rx { BackgroundTaskResult::MoreToDo } else { BackgroundTaskResult::Complete };
+        return if tx || rx {
+            BackgroundTaskResult::MoreToDo
+        } else {
+            BackgroundTaskResult::Complete
+        };
     }
 
-    /// get_available_data: 
+    /// get_available_data:
     /// returns a struct containing up to 64 bytes of available data.
     fn get_available_data(&mut self) -> RxStruct {
-
-        if self.rx_consumer.is_empty() {
+        if 0 == self.rx_count {
             // no data received so return an empty array and a count of 0
-            return RxStruct{BytesAvailable: 0, DataArray: [0u8; 64]};
+            return RxStruct {
+                bytes_available: 0,
+                data_array: [0u8; 64],
+            };
         }
 
         let mut result = [0u8; 64];
         let mut count: u8 = 0;
 
+        let mut rx_data = self.get_byte_from_rx_buffer();
+
         // pop off up to 64 bytes of received data.
-        while count < 64 && !self.rx_consumer.is_empty() {
-            result[count as usize] = self.rx_consumer.pop().unwrap();
+        while count < 64 && rx_data.0 {
+            result[count as usize] = rx_data.1;
             count += 1;
+            rx_data = self.get_byte_from_rx_buffer();
         }
 
         // and return the data
-        return RxStruct{BytesAvailable: count, DataArray: result};
+        return RxStruct {
+            bytes_available: count,
+            data_array: result,
+        };
     }
 
     /// set_data_to_transmit:
-    /// add the passed data to the transmit buffer, to be sent out 
+    /// add the passed data to the transmit buffer, to be sent out
     /// as part of the background task
     fn set_data_to_transmit(&mut self, data: [u8; 64], count: u8) {
-
         // push all the data to the tx buffer
         for b in data[0..count as usize].iter() {
-            self.tx_producer.push(*b);
-        } 
+            self.add_byte_to_tx_buffer(*b);
+        }
     }
 }
--- volks-fw/src/main.rs
+++ volks-fw/src/main.rs
@@ -11,15 +11,20 @@ mod app {
     #[cfg(feature = "heap")]
     use {crate::consts::HEAP_SIZE, cortex_m_rt};
     use {
+        hal::{
+            clocks::{Clocks, ExternalOscillator, Internal, LfOscStopped},
+            usbd::{UsbPeripheral, Usbd},
+        },
         nrf52840_hal as hal,
-        pc_communications_hal::pc_coms_hal::{HardwareInterface, PcInterfaceTrait, PcInterface, RxStruct},
+        pc_communications_hal::pc_coms_hal::{
+            HardwareInterface, PcInterface, PcInterfaceTrait, RxStruct,
+        },
         rtt_target::{rprintln, rtt_init_print},
         systick_monotonic::*,
-        hal::{
-            usbd::{Usbd, UsbPeripheral}, 
-            clocks::{Clocks, ExternalOscillator, Internal, LfOscStopped}
+        usb_device::{
+            bus::UsbBusAllocator,
+            device::{UsbDeviceBuilder, UsbVidPid},
         },
-        usb_device::{bus::UsbBusAllocator, device::{UsbDeviceBuilder, UsbVidPid}},
         usbd_serial::{SerialPort, USB_CLASS_CDC},
     };
 
@@ -38,9 +43,10 @@ mod app {
     fn init(cx: init::Context) -> (Shared, Local, init::Monotonics) {
         rtt_init_print!();
         rprintln!("[Init]");
-        
+
         // Initialise the allocator if heap is enabled.
-        #[cfg(feature = "heap")] {
+        #[cfg(feature = "heap")]
+        {
             rprintln!("Initialising heap allocation!");
             unsafe {
                 base::ALLOCATOR.init(cortex_m_rt::heap_start() as usize, HEAP_SIZE);
@@ -52,40 +58,36 @@ mod app {
         // initialise the USB and serial port peripherals
         static mut CLOCKS: Option<Clocks<ExternalOscillator, Internal, LfOscStopped>> = None;
         static mut USB_BUS: Option<UsbBusAllocator<Usbd<UsbPeripheral<'static>>>> = None;
-        
+
         let clocks = Clocks::new(cx.device.CLOCK).enable_ext_hfosc();
         let usbd = cx.device.USBD;
 
         unsafe {
-            CLOCKS = Some(clocks); 
+            CLOCKS = Some(clocks);
             let clocks = CLOCKS.as_ref().unwrap();
             USB_BUS = Some(Usbd::new(UsbPeripheral::new(usbd, &clocks)));
             let usb_bus = USB_BUS.as_ref().unwrap();
-            let mut serial_port = SerialPort::new(&usb_bus);
-            let mut usb_dev = UsbDeviceBuilder::new(usb_bus, UsbVidPid(0x16c0, 0x27dd))
+            let serial_port = SerialPort::new(&usb_bus);
+            let usb_dev = UsbDeviceBuilder::new(usb_bus, UsbVidPid(0x16c0, 0x27dd))
                 .manufacturer("Volks EEG")
                 .product("Serial port")
                 .serial_number("TEST")
                 .device_class(USB_CLASS_CDC)
                 .max_packet_size_0(64) // (makes control transfers 8x faster)
-                .build();    
-                
+                .build();
+
             // pass the usb device and serial port to the pc interface
-            let pc_interface = PcInterface::new(HardwareInterface{usb_dev, serial_port});
-        
-            (
-                Shared {},
-                Local {
-                    pc_interface,
-                },
-                init::Monotonics(mono),
-            )   
+            let pc_interface = PcInterface::new(HardwareInterface {
+                usb_dev,
+                serial_port,
+            });
+
+            (Shared {}, Local { pc_interface }, init::Monotonics(mono))
         }
     }
 
     #[idle(local = [pc_interface])]
     fn idle(mut _ctx: idle::Context) -> ! {
-
         // local copy of the pc interface
         let pc = _ctx.local.pc_interface;
 
@@ -96,8 +98,8 @@ mod app {
             let rx: RxStruct = pc.get_available_data();
 
             // if there is data then transmit if back out
-            if rx.BytesAvailable > 0 {
-                pc.set_data_to_transmit(rx.DataArray, rx.BytesAvailable);
+            if rx.bytes_available > 0 {
+                pc.set_data_to_transmit(rx.data_array, rx.bytes_available);
             }
         }
     }
