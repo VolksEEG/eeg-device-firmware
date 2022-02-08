39c0ad5d29a47fa41f2abb25cb549c9d12d07bfe
Longevity Software <lognevity_software@mail.com>
2022-02-08 20:44:10 +0000

    Capturing some planning of high level driver. #009

--- lib/ads1299_driver/README.md
+++ lib/ads1299_driver/README.md
@@ -1,6 +1,10 @@
 # ADS1299 Driver
 ## Overview
 This driver consists of both a high level driver which exposes the following abstracted functionality:
-* Starting / Stopping Data Capture
-* 
+* Retrieving the number of available channels
+* Starting / Stopping continuous data capture.
+* Configuring the input channels.
+* Enabling / disabling current sources.
+* Setting / Getting GPIO levels.
+
 And a low level driver which is called by the high level driver to set the appropriate register values in the ADS1299.
--- test/test_Ads1299Driver/test_Ads1299Driver.cpp
+++ test/test_Ads1299Driver/test_Ads1299Driver.cpp
@@ -2,14 +2,41 @@
 #include <Ads1299Driver.h>
 #include <unity.h>
 
-void test_Ads1299Driver_DeviceIsResetOnInitialisation(void)
+void test_Ads1299Driver_DeviceIsResetAndNumberOfChannelsIsRequestedOnInitialisation(void)
 {
-    Ads1299Driver uut = Ads1299Driver();
+    // Given 
+    // uut is not instantiated
+
+    // When
+    // uut is instantiated
+
+    // then 
+    // the following functions will be called in the low level driver
+
+    // Reset Device
+    // Get Number Of Channels
+
+    TEST_IGNORE_MESSAGE("Ignoring this test until the low level driver is implemented");
+}
+
+void test_Ads1299Driver_SetActiveChannelsAboveMaximumIsIgnored(void)
+{
+    // Given 
+    // The returned number of channels is 4 or 6.
+
+    // when 
+    // The user attempts to set a channel above this value.
+
+    // Then
+    // Only the lower 4 or 6 channels are passed to the low level driver.
+
+    TEST_IGNORE_MESSAGE("Ignoring this test until the low level driver is implemented");
 }
 
 int main(int argc, char **argv) {
     UNITY_BEGIN();
-    RUN_TEST(test_Ads1299Driver_DeviceIsResetOnInitialisation);
+    RUN_TEST(test_Ads1299Driver_DeviceIsResetAndNumberOfChannelsIsRequestedOnInitialisation);
+    RUN_TEST(test_Ads1299Driver_SetActiveChannelsAboveMaximumIsIgnored);
     UNITY_END();
 
     return 0;
